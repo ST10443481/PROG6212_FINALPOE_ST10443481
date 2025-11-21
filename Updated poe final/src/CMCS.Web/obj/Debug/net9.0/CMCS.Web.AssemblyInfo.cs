89fd4871f25daea78e3ef08e073fb02d9146a3ef
Futwa, Thando <164188075+ST10443481@users.noreply.github.com>
2025-11-21 20:10:05 +0200

    commit 10
    final changes

--- Updated poe final/src/CMCS.Web/obj/Debug/net9.0/CMCS.Web.AssemblyInfo.cs
+++ Updated poe final/src/CMCS.Web/obj/Debug/net9.0/CMCS.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1af8e4769603644856eb6b55fadd7cc8044bc75e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d06cbb8517c6a30cfc4fdcd486388c52c4bd0c60")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
